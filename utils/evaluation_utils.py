83d0262a857c54103c4b89efdb7130cff2c32afb
praveen-iyer <apraveen1999@gmail.com>
2023-03-25 23:44:18 -0700

    improved LP: still not working

--- utils/evaluation_utils.py
+++ utils/evaluation_utils.py
@@ -46,46 +46,39 @@ def tupler(l):
 def patchify_tensor(t):
     # Expects an image like tensor (C,H,W)
     # Returns list of 3x3 patches
-    c,h,w = t.shape
-    patches = []
-    for i in range(h-2):
-        for j in range(w-2):
-            patches.append(t[:,i:i+3,j:j+3])
+    h,w,c = t.shape
+    # patches = []
+    # for i in range(h-2):
+    #     for j in range(w-2):
+    #         patches.append(t[i:i+3,j:j+3,:])
+    out_shape = (h-2,w-2,3,3,c)
+    patches = np.lib.stride_tricks.as_strided(t, shape = out_shape, strides=(t.strides[0],t.strides[1],t.strides[0],t.strides[1],t.strides[2]))
     return patches
 
-def get_normalized_cross_correlation_measure(stylized_feature_map, style_feature_map):
+def get_normalized_cross_correlation_measure_and_layer_lp2(stylized_feature_map, style_feature_map):
+    stylized_feature_map = stylized_feature_map.detach().cpu().numpy().transpose((1,2,0))
+    style_feature_map = style_feature_map.cpu().numpy().transpose((1,2,0))
     stylized_patches = patchify_tensor(stylized_feature_map)
     style_patches = patchify_tensor(style_feature_map)
+
+    unique_style_patches = {tupler(style_patches[i][j].tolist()) for j in range(style_patches.shape[1]) for i in range(style_patches.shape[0])}
+    unique_max_patches = set()
 
     normalized_cross_correlation_measure = 0
     for stylized_patch in stylized_patches:
         max_val = float('-inf')
         for style_patch in style_patches:
-            normalized_cross_correlation_measure += np.sum(np.multiply(stylized_patch.detach().cpu().numpy(), style_patch.cpu().numpy()))/(np.linalg.norm(stylized_patch.detach().cpu().numpy())*np.linalg.norm(style_patch.cpu().numpy()))
-            max_val = max(max_val, normalized_cross_correlation_measure)
-        normalized_cross_correlation_measure += max_val
-
-    normalized_cross_correlation_measure /= len(stylized_patches)
-    return normalized_cross_correlation_measure
-
-def calc_layer_lp2(stylized_feature_map, style_feature_map):
-    stylized_patches = patchify_tensor(stylized_feature_map)
-    style_patches = patchify_tensor(style_feature_map)
-
-    unique_style_patches = {tupler(style_patch.tolist()) for style_patch in style_patches}
-    unique_max_patches = set()
-
-    for stylized_patch in stylized_patches:
-        max_val = float('-inf')
-        for style_patch in style_patches:
-            normalized_cross_correlation_measure += np.sum(np.multiply(stylized_patch.detach().cpu().numpy(), style_patch.cpu().numpy()))/(np.linalg.norm(stylized_patch.cpu().numpy())*np.linalg.norm(style_patch.cpu().numpy()))
+            normalized_cross_correlation_measure += np.sum(np.multiply(stylized_patch, style_patch))/(np.linalg.norm(stylized_patch)*np.linalg.norm(style_patch))
             if max_val<normalized_cross_correlation_measure:
                 max_val = normalized_cross_correlation_measure
                 max_patch = style_patch
+            max_val = max(max_val, normalized_cross_correlation_measure)
         unique_max_patches.add(tupler(max_patch).tolist())
         normalized_cross_correlation_measure += max_val
 
-    return len(unique_max_patches)/len(unique_style_patches)
+    normalized_cross_correlation_measure /= len(stylized_patches)
+    layer_lp2 = len(unique_max_patches)/len(unique_style_patches)
+    return normalized_cross_correlation_measure, layer_lp2
 
 def local_patterns(stylized_image, style_image, config, device):
     neural_net, _, style_feature_maps_indices_names = prepare_model(config['model'], device)
@@ -93,12 +86,10 @@ def local_patterns(stylized_image, style_image, config, device):
     style_img_set_of_feature_maps = neural_net(style_image)
     stylized_img_layer_of_feature_maps = [x for cnt, x in enumerate(stylized_img_set_of_feature_maps) if cnt in style_feature_maps_indices_names[0]]
     style_img_layer_of_feature_maps = [x for cnt, x in enumerate(style_img_set_of_feature_maps) if cnt in style_feature_maps_indices_names[0]]
-    temp_lp1 = [get_normalized_cross_correlation_measure(stylized_feature_map, style_feature_map) 
-                for stylized_feature_map, style_feature_map in zip(stylized_img_layer_of_feature_maps, style_img_layer_of_feature_maps)]
+    temp_lp1, temp_lp2 = zip(*[get_normalized_cross_correlation_measure_and_layer_lp2(stylized_feature_map, style_feature_map) 
+                for stylized_feature_map, style_feature_map in zip(stylized_img_layer_of_feature_maps, style_img_layer_of_feature_maps)])
+    
     lp1 = sum(temp_lp1)/len(temp_lp1)
-
-    temp_lp2 = [calc_layer_lp2(stylized_feature_map, style_feature_map) 
-               for stylized_feature_map, style_feature_map in zip(stylized_img_layer_of_feature_maps, style_img_layer_of_feature_maps)]
     lp2 = sum(temp_lp2)/len(temp_lp2)
 
     lp = (lp1+lp2)/2
